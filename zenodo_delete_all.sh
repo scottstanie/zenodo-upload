236402b093bd62f5b7f3ba81cc761eccc7264d22
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-04-18 07:37:14 -0700

    add --sandbox option to upload to sandbox.zenodo

--- zenodo_delete_all.sh
+++ zenodo_delete_all.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 # Deletes all files of a Zenodo deposition.
 #
-# usage: ./zenodo_delete_all.sh [deposition id] 
+# usage: ./zenodo_delete_all.sh deposition-id [--sandbox]
 #
 
 set -e
+USAGE="$0 deposition-id [--sandbox]"
 
-# strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
-DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
+# Check they have provided a deposition id and a file
+if [[ $# -lt 1 ]]; then
+    echo "usage: $USAGE"
+    exit 1
+fi
 
-curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files" | jq --raw-output .[].id > file_ids.txt
+URL_BASE="www.zenodo"
 
-cat file_ids.txt | parallel "echo curl -XDELETE -H '@<(echo -e \"Authorization: Bearer $ZENODO_TOKEN\")' \\\"https://www.zenodo.org/api/deposit/depositions/$DEPOSITION/files/{1}\\\"" > delete_cmds.sh
+# Check if the user added --sandbox, if so adjust the url
+if [[ $# -gt 1 ]] && [[ $2 == "--sandbox" ]]; then
+    URL_BASE="sandbox.zenodo"
+fi
+
+DEPOSITION="$1"
+echo "https://${URL_BASE}.org/api/deposit/depositions/${DEPOSITION}/files" 
+curl -H @<(echo -e "Accept: application/json\nAuthorization: Bearer $ZENODO_TOKEN") "https://${URL_BASE}.org/api/deposit/depositions/${DEPOSITION}/files" | jq --raw-output .[].id > file_ids.txt
+
+cat file_ids.txt | parallel "echo curl -XDELETE -H '@<(echo -e \"Authorization: Bearer $ZENODO_TOKEN\")' \\\"https://${URL_BASE}.org/api/deposit/depositions/$DEPOSITION/files/{1}\\\"" > delete_cmds.sh
--- zenodo_upload.sh
+++ zenodo_upload.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 # Upload big files to Zenodo.
 #
-# usage: ./zenodo_upload.sh [deposition id] [filename]
+# usage: ./zenodo_upload.sh deposition-id filename [--sandbox]
 #
 
 set -e
 
-# strip deposition url prefix if provided; see https://github.com/jhpoelen/zenodo-upload/issues/2#issuecomment-797657717
-DEPOSITION=$( echo $1 | sed 's+^http[s]*://zenodo.org/deposit/++g' )
+USAGE="$0 deposition-id filename [--sandbox]"
+
+# Check they have provided a deposition id and a file
+if [[ $# -lt 2 ]]; then
+    echo "usage: $USAGE"
+    exit 1
+fi
+
+URL_BASE="www.zenodo"
+
+# Check if the user added --sandbox, if so adjust the url
+if [[ $# -gt 2 ]] && [[ $3 == "--sandbox" ]]; then
+    URL_BASE="sandbox.zenodo"
+fi
+
+DEPOSITION="$1"
 FILEPATH="$2"
 FILENAME=$(echo $FILEPATH | sed 's+.*/++g')
 
-BUCKET=$(curl https://zenodo.org/api/deposit/depositions/"$DEPOSITION"?access_token="$ZENODO_TOKEN" | jq --raw-output .links.bucket)
+DEPO_URL="https://$URL_BASE.org/api/deposit/depositions/$DEPOSITION"
+echo "Uploading $FILEPATH to $DEPO_URL"
+BUCKET=$(curl $DEPO_URL?access_token="$ZENODO_TOKEN" | jq --raw-output .links.bucket)
 
 curl --progress-bar -o /dev/null --upload-file "$FILEPATH" $BUCKET/"$FILENAME"?access_token="$ZENODO_TOKEN"
